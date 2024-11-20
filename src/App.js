08aeb0f8a60f3475fb1b3bc16ac61108c4166ec9
Shubham Verma <shubhamv2010@gmail.com>
2024-11-20 12:51:20 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
-import About from "./components/About";
+// import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
