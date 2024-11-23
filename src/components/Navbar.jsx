d6b467fd4148edc6d8b1ca98dfe62d4fef965ab9
Shubham Verma <shubhamv2010@gmail.com>
2024-11-23 15:48:07 +0530

    Projects Disabled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FiMenu, FiX, FiHome, FiUser, FiBriefcase, FiMail, FiSun, FiMoon } from "react-icons/fi";
+import { FiMenu, FiX, FiHome, FiUser, FiMail, FiSun, FiMoon } from "react-icons/fi";
 
 const Navbar = ({ toggleTheme }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
